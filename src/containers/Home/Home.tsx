2bf2166ae29cd1bc4fb9d63a24c5b79124d8af34
Karina Ledesma <ledesmakv@gmail.com>
2022-03-05 20:41:18 -0300

    Adds styled-components and styles

--- src/containers/Home/Home.tsx
+++ src/containers/Home/Home.tsx
@@ -1,6 +1,7 @@
-import { BarDatum } from '@nivo/bar';
-import React, { useState } from 'react'
-import { PowerBar } from './components/PowerBar.tsx/PowerBar';
+import { BarDatum } from "@nivo/bar";
+import React, { useState } from "react";
+import { PowerBar } from "./components/PowerBar.tsx/PowerBar";
+import { Background, Button, HomeTitle } from "../Home.styles";
 
 export interface PowerDatapoint extends BarDatum {
   time: number;
@@ -8,49 +9,59 @@ export interface PowerDatapoint extends BarDatum {
 }
 
 export function Home() {
-  const [pairedDevice, setPairedDevice] = useState(false)
-  const [currentPower, setCurrentPower] = useState(0)
-  const [historicalPower, setHistoricalPower] = useState<PowerDatapoint[]>([])
+  const [pairedDevice, setPairedDevice] = useState(false);
+  const [currentPower, setCurrentPower] = useState(0);
+  const [historicalPower, setHistoricalPower] = useState<PowerDatapoint[]>([]);
 
   const pushPowerDatapoint = (power: number) => {
-    setCurrentPower(power)
-    setHistoricalPower((powerHistory: any) => [...powerHistory, {
-      time: powerHistory.length + 1,
-      power
-    }])
-  }
+    setCurrentPower(power);
+    setHistoricalPower((powerHistory: any) => [
+      ...powerHistory,
+      {
+        time: powerHistory.length + 1,
+        power,
+      },
+    ]);
+  };
 
   const pairDevice = () => {
-    (navigator as any).bluetooth.requestDevice({
-      filters: [{ services: ["cycling_power"] }]
-    })
+    (navigator as any).bluetooth
+      .requestDevice({
+        filters: [{ services: ["cycling_power"] }],
+      })
       .then((device: any) => {
-        return device.gatt.connect()
+        return device.gatt.connect();
       })
       .then(async (server: any) => {
-        setPairedDevice(true)
-        const service = await server.getPrimaryService("00001818-0000-1000-8000-00805f9b34fb")
-        const characteristics = await service.getCharacteristics()
+        setPairedDevice(true);
+        const service = await server.getPrimaryService(
+          "00001818-0000-1000-8000-00805f9b34fb"
+        );
+        const characteristics = await service.getCharacteristics();
         await characteristics[0].startNotifications();
-        characteristics[0].addEventListener('characteristicvaluechanged', (e: any) => {
-          pushPowerDatapoint(e.target!.value.getInt8(2))
-        })
+        characteristics[0].addEventListener(
+          "characteristicvaluechanged",
+          (e: any) => {
+            pushPowerDatapoint(e.target!.value.getInt8(2));
+          }
+        );
       })
       .catch((error: Error) => {
-        setPairedDevice(false)
+        setPairedDevice(false);
         console.error(error);
       });
-  }
+  };
+
   return (
-    <div>
-      <div>Home</div>
-      <button onClick={pairDevice}>Pair bluetooth power meter</button>
+    <Background>
+      <HomeTitle>Home</HomeTitle>
+      <Button onClick={pairDevice}>Pair bluetooth power meter</Button>
       <br />
       <span>Paired device: {pairedDevice ? "true" : "false"}</span>
       <br />
       <span>Current Power: {currentPower}</span>
       <br />
       <PowerBar data={historicalPower} />
-    </div>
-  )
+    </Background>
+  );
 }
